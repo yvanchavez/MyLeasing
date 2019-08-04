084ec673d83cbd9fde8c830e4c1d717f6e800342
yvan <chavez.yvan@gmail.com>
2019-08-03 22:04:31 -0500

    the owner class was created

---
+++ MyLeasing.Web/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyLeasing.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLeasing.Web.Data
+{
+    public class DataContext : DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Owner> Owners { get; set; }
+
+    }
+}
